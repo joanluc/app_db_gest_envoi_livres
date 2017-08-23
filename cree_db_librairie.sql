3a14c9b6b69919cd57e5a2f29263904e9e82e40b
joanluc <joanluc@pc-p-jola2.crpp-bordeaux.cnrs.fr>
2017-08-24 01:01:04 +0200

    add app_db_gest_envoi_livres.py contact.py cree_db_librairie.sql docs/ entreprise.py qt_gest_envoi_livres.py

---
+++ cree_db_librairie.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE "Librairie";
+
+GRANT ALL ON "Librairie" TO "Librairie" WITH GRANT OPTION;
+
+GRANT ALL ON "Librairie" TO "Morgane" WITH GRANT OPTION;
+
+GRANT ALL ON "Librairie" TO postgres;
