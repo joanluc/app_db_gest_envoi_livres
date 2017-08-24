58db85077a6f5236b108203363d8364a63c48bdb
joanluc <joanluc@users.noreply.github.com>
2017-08-24 13:52:45 +0200

    Update cree_db_librairie.sql

--- cree_db_librairie.sql
+++ cree_db_librairie.sql
@@ -1,7 +1,12 @@
-CREATE DATABASE "Librairie";
+CREATE DATABASE "Librairie"
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'French_France.1252'
+    LC_CTYPE = 'French_France.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
 
 GRANT ALL ON "Librairie" TO "Librairie" WITH GRANT OPTION;
 
 GRANT ALL ON "Librairie" TO "Morgane" WITH GRANT OPTION;
-
-GRANT ALL ON "Librairie" TO postgres;
